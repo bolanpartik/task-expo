aaf82567bd41b05ad000e3b70c69502584c22831
Partik Bolan <bolanpartik@gmail.com>
2024-09-08 01:12:25 +0530

    add styles for NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 export default function NavBar(){
     return(
-        <div>
-            <div>
-                <p>Todo-Expo</p>
+        <div className='h-16 flex justify-between bg-black/30 backdrop-blur-xl text-customCyan border-b border-customGray'>
+            <div className='flex items-center ml-8'>
+                <p className='text-3xl font-bold'>Todo-Expo</p>
             </div>
-            <div>
-                <a href='#'>Home</a>
-                <a href='#'>Contact</a>
-                <a href='#'>Support</a>
-                <a href='#'>About</a>
+            <div className='flex gap-5 items-center mr-6'>
+                <a href='#' className='underline-offset-4 hover:underline hover:scale-110 decoration-yellow-500 transition-all'>Home</a>
+                <a href='#' className='underline-offset-4 hover:underline hover:scale-110 decoration-yellow-500 transition-all'>Contact</a>
+                <a href='#' className='underline-offset-4 hover:underline hover:scale-110 decoration-yellow-500 transition-all'>Support</a>
+                <a href='#' className='underline-offset-4 hover:underline hover:scale-110 decoration-yellow-500 transition-all'>About</a>
             </div>
         </div>
     )
