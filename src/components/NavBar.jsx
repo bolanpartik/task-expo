824b5589162ee3125aa4208915bae7fc14c41074
Partik Bolan <bolanpartik@gmail.com>
2024-09-07 20:33:40 +0530

    add NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+export default function NavBar(){
+    return(
+        <div>
+            <div>
+                <p>Todo-Expo</p>
+            </div>
+            <div>
+                <a href='#'>Home</a>
+                <a href='#'>Contact</a>
+                <a href='#'>Support</a>
+                <a href='#'>About</a>
+            </div>
+        </div>
+    )
+}
