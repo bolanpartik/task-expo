9e2a92fe83edf1d0a8770adabf844942763934b6
Partik Bolan <bolanpartik@gmail.com>
2024-09-08 18:53:52 +0530

    add ref to store total no. of todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import NavBar from "./components/NavBar"
 import AddTodo from './components/AddTodo'
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 function App() {
 
+  const todoRef = useRef(0)
   const [allTodos, setAllTodos] = useState([])
 
   return (
     <div>
       <NavBar />
-      <AddTodo setAllTodos={setAllTodos} />
+      <AddTodo setAllTodos={setAllTodos} todoRef={todoRef} />
     </div>
   )
 }
