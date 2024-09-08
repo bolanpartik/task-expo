dd3652275c24d9c063a0696c609c393ffa4f926a
Partik Bolan <bolanpartik@gmail.com>
2024-09-08 18:33:18 +0530

    add AddTodo component

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,15 @@
+export default function AddTodo() {
+
+    return (
+        <div>
+            <p>New Task Entry</p>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter your task"
+                />
+                <button>Add Todo</button>
+            </div>
+        </div>
+    );
+}
