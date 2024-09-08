05fc11153de66e43e3c5f59ba74f311a5b0200c9
Partik Bolan <bolanpartik@gmail.com>
2024-09-08 18:50:06 +0530

    update userInput on input change

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -4,12 +4,18 @@ export default function AddTodo({ setAllTodos }) {
 
     const [userInput, setUserInput] = useState('');
 
+    const handleInputChange=(event)=>{
+        setUserInput(event.target.value)
+    }
+
     return (
         <div>
             <p>New Task Entry</p>
             <div>
                 <input
                     type="text"
+                    value={userInput}
+                    onChange={handleInputChange}
                     placeholder="Enter your task"
                 />
                 <button>Add Todo</button>
