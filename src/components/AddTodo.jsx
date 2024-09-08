37039c5cfd64c77ab92d9f76790a33d281963ac4
Partik Bolan <bolanpartik@gmail.com>
2024-09-08 22:44:11 +0530

    add styles for AddTodo component

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -5,12 +5,12 @@ export default function AddTodo({ setAllTodos, todoRef }) {
     const [userInput, setUserInput] = useState('');
 
     const handleAddTodo = () => {
-        
+
         if (userInput == null || userInput === '') {
             alert('Please enter a valid Todo.');
             return;
         }
-        
+
         setAllTodos(prevTodos => [
             ...prevTodos, {
                 title: userInput,
@@ -23,21 +23,26 @@ export default function AddTodo({ setAllTodos, todoRef }) {
         setUserInput('');
     };
 
-    const handleInputChange=(event)=>{
+    const handleInputChange = (event) => {
         setUserInput(event.target.value)
     }
 
     return (
-        <div>
-            <p>New Task Entry</p>
-            <div>
+        <div className="flex flex-col items-center gap-5 mt-5">
+            <p className="text-3xl">New Task Entry</p>
+            <div className="flex gap-4">
                 <input
                     type="text"
                     value={userInput}
                     onChange={handleInputChange}
+                    className="bg-[#22272e] h-10 w-80 rounded-lg ring-2 px-2 ring-gray-700 focus:placeholder:opacity-40 focus:ring-sky-600 focus:outline-none focus:bg-customBlack"
                     placeholder="Enter your task"
                 />
-                <button onClick={handleAddTodo}>Add Todo</button>
+                <button
+                    onClick={handleAddTodo}
+                    className="bg-black p-2 px-4 border-none outline-none text-customCyan rounded-md shadow hover:shadow-white/30"
+                    >Add Todo
+                </button>
             </div>
         </div>
     );
