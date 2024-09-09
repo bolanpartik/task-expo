cec89e0698d2ff34a7900931a09edbdbebd7cca9
Partik Bolan <bolanpartik@gmail.com>
2024-09-10 00:42:03 +0530

    effect to globally change todo status on checkbox change

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function TodoItem({ allTodos, id, setAllTodos }) {
 
     const [currTodo, setCurrTodo] = useState(allTodos.find(t => t.id ==id))
     const [isTodoCompleted, setIsTodoCompleted] = useState(currTodo.isCompleted)
     
+    useEffect(() => {
+        setCurrTodo((prevTodo) => ({ ...prevTodo, isCompleted: isTodoCompleted }));
+    }, [isTodoCompleted]);
+
     const handleChange = () => {
         setIsTodoCompleted(prev => !prev)
     }
