bdfbafd1eb8c796ce58abd541d630c2e8044cc45
Partik Bolan <bolanpartik@gmail.com>
2024-09-09 23:49:12 +0530

    state to get current todo

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 export default function TodoItem({ allTodos, id, setAllTodos }) {
+    
+    const [currTodo, setCurrTodo] = useState(allTodos.find(t => t.id ==id))
 
     return (
         <div >
