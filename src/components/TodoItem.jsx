2e131c7302fe150d6f1749907467081f61631e30
Partik Bolan <bolanpartik@gmail.com>
2024-09-10 01:39:20 +0530

    add styles for TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -33,19 +33,26 @@ export default function TodoItem({ allTodos, id, setAllTodos }) {
     }
 
     return (
-        <div>
-            <input
-                type="checkbox"
-                checked={isTodoCompleted}
-                onChange={handleChange}
-            />
-            <p>{currTodo.title}</p>
-            <SquarePen
-                onClick={handleEdit}
-            />
-            <Trash2
-                onClick={handleDelete}
-            />
+        <div className="flex bg-customGray w-full rounded-md p-3 justify-between transition-all duration-300 hover:outline outline-1 outline-blue-600 items-center">
+            <div className="flex ml-3 gap-3 w-1/2">
+                <input
+                    type="checkbox"
+                    checked={isTodoCompleted}
+                    onChange={handleChange}
+                    className="w-4 accent-green-500 transition-all"
+                />
+                <p className="text-lg font-medium w-full transition-all">{currTodo.title}</p>
+            </div>
+            <div className="flex w-1/4 justify-between">
+                <SquarePen
+                    onClick={handleEdit}
+                    className="hover:text-green-500 hover:scale-95"
+                />
+                <Trash2
+                    onClick={handleDelete}
+                    className="mr-3 hover:text-red-600 hover:scale-95"
+                />
+            </div>
         </div>
     )
 }
