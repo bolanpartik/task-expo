d9b87bc0e9bfe10a619eee912dfc3807073487b2
Partik Bolan <bolanpartik@gmail.com>
2024-09-09 22:07:47 +0530

    render TodoItem in TodoCard for each todo

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,8 +1,19 @@
-export default function TodoCard() {
+import TodoItem from "./TodoItem"
+
+export default function TodoCard({ allTodos, setAllTodos,todoRef}) {
 
     return (
         <div>
             <p>Your Tasks </p>
+            {allTodos?.map((todo) => {
+                return <TodoItem
+                            key={todo.id}
+                            allTodos={allTodos}
+                            setAllTodos={setAllTodos}
+                            id={todo.id}
+                        />
+                })
+            }
         </div>
     )
 }
