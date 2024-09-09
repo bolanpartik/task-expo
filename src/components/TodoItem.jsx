459cfca32cb80a6275a7a293dc521e1c052ee981
Partik Bolan <bolanpartik@gmail.com>
2024-09-10 00:39:18 +0530

    change todo status by click on checkbox

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,12 +4,17 @@ export default function TodoItem({ allTodos, id, setAllTodos }) {
 
     const [currTodo, setCurrTodo] = useState(allTodos.find(t => t.id ==id))
     const [isTodoCompleted, setIsTodoCompleted] = useState(currTodo.isCompleted)
+    
+    const handleChange = () => {
+        setIsTodoCompleted(prev => !prev)
+    }
 
     return (
         <div>
             <input
                 type="checkbox"
                 checked={isTodoCompleted}
+                onChange={handleChange}
             />
             <p>{currTodo.title}</p>
         </div>
