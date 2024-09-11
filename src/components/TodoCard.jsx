54075a4ea1dc0589017c6d41b983d5abcd4a0b70
Partik Bolan <bolanpartik@gmail.com>
2024-09-11 07:51:55 +0530

    add styles for TodoCard component

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -8,11 +8,12 @@ export default function TodoCard({ allTodos, setAllTodos,todoRef}) {
     }
 
     return (
-        <div>
+        <div className="flex flex-col justify-center w-1/2 p-3 items-center mt-5 bg-black/40 gap-4 rounded-lg">
             {allTodos.length > 0 ?
-                <div>
+                <div className="flex justify-between w-full px-4">
                     <p>Your Tasks </p>
                     <button
+                        className="outline-none border-none hover:bg-customGray rounded-sm px-1"
                         onClick={handleDeleteAll}>Delete All
                     </button>
                 </div>
