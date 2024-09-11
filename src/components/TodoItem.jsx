496955db7da1e4c4b3337c71128d1155a110ec6b
Partik Bolan <bolanpartik@gmail.com>
2024-09-12 02:07:21 +0530

    improve styles

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -37,7 +37,7 @@ export default function TodoItem({ allTodos, id, setAllTodos }) {
     }
 
     return (
-        <div className="flex bg-customGray w-full rounded-md p-3 justify-between transition-all duration-300 hover:outline outline-1 outline-blue-600 items-center" onClick={handleChange}>
+        <div className={`flex bg-customGray w-full rounded-md p-3 justify-between hover:outline outline-1 outline-blue-600 items-center ${isTodoCompleted?'opacity-60':'opacity-100'}`} onClick={handleChange}>
             <div className="flex ml-3 gap-3 w-1/2">
                 <input
                     type="checkbox"
@@ -45,7 +45,7 @@ export default function TodoItem({ allTodos, id, setAllTodos }) {
                     onChange={handleChange}
                     className="w-4 accent-green-500 transition-all"
                 />
-                <p className="text-lg font-medium w-full transition-all" style={{ textDecoration: isTodoCompleted ? 'line-through' : 'none' }}>{currTodo.title}</p>
+                <p className={`text-lg font-medium w-full transition-all ${isTodoCompleted?'line-through':'no-underline'}`}>{currTodo.title}</p>
             </div>
 
             {isTodoCompleted && <CheckCheck className="text-green-500"/>}
