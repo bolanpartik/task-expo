56291f790baf2892dad5f7b968d7c99225e20c7e
Partik Bolan <bolanpartik@gmail.com>
2024-09-20 22:40:32 +0530

    add theme support

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,25 @@ function App() {
 
   const todoRef = useRef(0)
   const [allTodos, setAllTodos] = useState([])
-  const [mode,setMode]=useState('dark')
+  const [mode, setMode] = useState('dark')
 
   return (
-    <div className="bg-gradient-to-b from-gray-800 to-black min-h-dvh text-customCyan select-none">
+    <div className={`${mode === "dark"
+                      ? "bg-gradient-to-b from-gray-800 to-black text-customCyan"
+                      : "bg-gradient-to-b from-gray-200 to-gray-600 text-gray-900"}
+                      h-full min-h-dvh max-h-dvh overflow-x-hidden select-none`}>
       <NavBar mode={mode} setMode={setMode}/>
       <div className="flex justify-center w-full items-center flex-col">
         <AddTodo
             setAllTodos={setAllTodos}
             todoRef={todoRef}
+            mode={mode}
         />
         <TodoCard
             allTodos={allTodos}
             setAllTodos={setAllTodos}
             todoRef={todoRef}
+            mode={mode}
         />
       </div>
     </div>
